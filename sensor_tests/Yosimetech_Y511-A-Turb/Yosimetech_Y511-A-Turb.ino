e6787399947ce061d218886ecffe093937590d2d
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2017-06-23 12:59:13 -0500

    Clean up Turbidity code

--- sensor_tests/Yosimetech_Y511-A-Turb/Yosimetech_Y511-A-Turb.ino
+++ sensor_tests/Yosimetech_Y511-A-Turb/Yosimetech_Y511-A-Turb.ino
@@ -2,7 +2,6 @@
 Modified by Anthony & Beth
 From sketch from YosemiTech for Y511 Turbidity with wiper
 ************/
-#include <Arduino.h>
 
 // Anthony note: Declare variables
 int State8 = LOW;
@@ -13,7 +12,11 @@ unsigned char buffer[13];   // Anthony Note: Allocate some space for the Bytes, 
 unsigned char command[16];  // Anthony Note: Allocate some space for the Bytes, as 16-element array of bytes
 float Temperature, VarXvalue;
 unsigned char startmeasure[8] = {0x01, 0x03, 0x25, 0x00, 0x00, 0x01, 0x8F, 0x06};
-unsigned char getTempandVarX[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};
+unsigned char getTempandVarX[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from Turbidity manual
+//unsigned char getTempandTurb[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from DO & Turbidity manual
+//unsigned char getTempandDO[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from DO & Turbidity manual
+//unsigned char getTempandChl[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x05, 0x8E, 0x81};  // from DO & Turbidity manual
+
 int i = 0;      // Anthony note: Index into array; where to store the Bytes
 int inbyte;     // Anthony note: Where to store the Bytes read
 String inputString = "";
@@ -26,11 +29,11 @@ union SeFrame {
 SeFrame Sefram;
 float Rev_float( unsigned char indata[], int stindex)
 {
-    Sefram.Byte[0] = indata[stindex];//Serial.read( );
-    Sefram.Byte[1] = indata[stindex + 1]; //Serial.read( );
-    Sefram.Byte[2] = indata[stindex + 2]; //Serial.read( );
-    Sefram.Byte[3] = indata[stindex + 3]; //Serial.read( );
-    return Sefram.Float;
+  Sefram.Byte[0] = indata[stindex];//Serial.read( );
+  Sefram.Byte[1] = indata[stindex + 1]; //Serial.read( );
+  Sefram.Byte[2] = indata[stindex + 2]; //Serial.read( );
+  Sefram.Byte[3] = indata[stindex + 3]; //Serial.read( );
+  return Sefram.Float;
 }
 
 float Rev_float(unsigned char indata[], int stindex);
@@ -38,77 +41,67 @@ float Rev_float(unsigned char indata[], int stindex);
 
 void setup()
 {
-    pinMode(8, OUTPUT);   // Anthony Note: LED2 green
-    pinMode(9, OUTPUT);   // Anthony Note: LED1 red
-    pinMode(22, OUTPUT);  // Anthony Note: switched power
-    digitalWrite(22, HIGH);
-    pinMode(12, OUTPUT);  // Anthony Note: ??
-    digitalWrite(12, LOW); // Anthony Note: ??
-    Serial.begin(9600);  // Anthony Note: this is the Mayfly's default USB port (UART-0)
-    Serial1.begin(9600); //this is the Mayfly's default Xbee port (UART-1)
-    //digitalWrite(12, HIGH);
-    delay(8);
-    Serial1.write(startmeasure, 8);///////////////////////////
-    //delay(12);
-    //digitalWrite(12, LOW);
-    delay(2000);  // recommended >2 second delay (see p 15 of manual) after Start Meaurement before Get values
+  pinMode(8, OUTPUT);   // Anthony Note: LED2 green
+  pinMode(9, OUTPUT);   // Anthony Note: LED1 red
+  pinMode(22, OUTPUT);  // Anthony Note: switched power. 5V to sensor, 3.3V to RS485 adaptor
+  digitalWrite(22, HIGH);
 
-    if (Serial1.available() > 0)
-    {
-        // read the incoming byte:
-        incomingByte = Serial1.readBytes(buffer, 13);
-    }
-    Serial.println("Temp(C)   TUR(NTU)");
+  Serial.begin(9600);  // Anthony Note: this is the Mayfly's default USB port (UART-0)
+  Serial1.begin(9600); //this is the Mayfly's default Xbee port (UART-1)
+
+  delay(8);
+  Serial1.write(startmeasure, 8);///////////////////////////
+
+  delay(2000);  // recommended >2 second delay (see p 15 of manual) after Start Meaurement before Get values
+
+  if (Serial1.available() > 0)
+  {
+    // read the incoming byte:
+    incomingByte = Serial1.readBytes(buffer, 13);
+  }
+  Serial.println("Temp(C)   TUR(NTU)");
 }
-
 
 void loop()
 {
-    // Anthony Note: Switch State8
-    if (State8 == LOW)
+  // Anthony Note: Switch State8
+  if (State8 == LOW)
+  {
+    State8 = HIGH;
+  }
+  else {
+    State8 = LOW;
+  }
+
+
+  digitalWrite(8, State8);  // Anthony Note: Turn on LED2 green if State8 is high
+  State9 = !State8;         // Anthony Note: Assign State9 to be NOT State8 (the opposite of State8)
+  digitalWrite(9, State9);  // Anthony Note: Turn on LED1 red if State9 is high
+
+  // send data only when you receive data:
+
+  if (Serial.available() > 0)
+  {
+    incomingByte = Serial.readBytes(command,17);
+    Serial1.write(command, incomingByte);
+  }
+  else
+    Serial1.write(getTempandVarX, 8);
+
+  delay(1000);
+
+  if (Serial1.available() > 0)
+  {
+    // read the incoming byte:
+    incomingByte = Serial1.readBytes(buffer, 13); //default to 1 second
+  // say what you got:
+    if (incomingByte == 13)
     {
-        State8 = HIGH;
+      Temperature = Rev_float(buffer, 3);
+      VarXvalue = Rev_float(buffer, 7);
+      Serial.print(Temperature, 2);
+      Serial.print("   ");
+      Serial.println(VarXvalue, 2);
     }
-    else {
-        State8 = LOW;
-    }
-
-    digitalWrite(8, State8);  // Anthony Note: Turn on LED2 green if State8 is high
-    State9 = !State8;         // Anthony Note: Assign State9 to be NOT State8 (the opposite of State8)
-    digitalWrite(9, State9);  // Anthony Note: Turn on LED1 red if State9 is high
-
-    // send data only when you receive data:
-    //digitalWrite(12, HIGH);
-    //delay(8);
-
-    if (Serial.available() > 0)
-    {
-        incomingByte = Serial.readBytes(command, 17);
-        // if ((incomingByte == 8)||(incomingByte == 17))
-        //{
-        Serial1.write(command, incomingByte);
-        //Serial.println("K     B");
-        // }
-    }
-    else Serial1.write(getTempandVarX, 8);
-
-    //delay(32);
-    //digitalWrite(12, LOW);
-    delay(2000);  // recommended >2 second delay (see p 15 of manual) after Start Meaurement before Get values
-
-    if (Serial1.available() > 0)
-    {
-        // read the incoming byte:
-        incomingByte = Serial1.readBytes(buffer, 13); //default to 1 second
-        // say what you got:
-        if (incomingByte == 13)
-        {
-            Temperature = Rev_float(buffer, 3);
-            VarXvalue = Rev_float(buffer, 7);
-            Serial.print(Temperature, 2);
-            Serial.print("   ");
-            Serial.println(VarXvalue, 2);
-        }
-        //Serial.print(buffer[0], HEX);
-    }
+  }
 }
