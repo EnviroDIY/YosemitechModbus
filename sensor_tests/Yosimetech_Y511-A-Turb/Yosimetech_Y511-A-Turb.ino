ab179eb22d67cab0334a7001416b461981f761be
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2017-06-26 10:42:30 -0500

    Aligned Cond & Turb sketches
    Removed cruft & unneccessary code.
    Aligned to similar indentation (two spaces per indent level).
    Conductivity uses get values command from manual, with 15 byte response, but output values are still 0.000000

--- sensor_tests/Yosimetech_Y511-A-Turb/Yosimetech_Y511-A-Turb.ino
+++ sensor_tests/Yosimetech_Y511-A-Turb/Yosimetech_Y511-A-Turb.ino
@@ -1,7 +1,9 @@
 /************
 Modified by Anthony & Beth
-From sketch from YosemiTech for Y511 Turbidity with wiper
+From sketch from YosemiTech for
+Y511 Turbidity with wiper
 ************/
+#include <Arduino.h>
 
 // Anthony note: Declare variables
 int State8 = LOW;
@@ -10,13 +12,14 @@ int incomingByte = 0; // for incoming serial data. Anthony Note: where to store 
 // Anthony Note: "unsigned char" datatype is equivalent to "byte". https://oscarliang.com/arduino-difference-byte-uint8-t-unsigned-cha/
 unsigned char buffer[13];   // Anthony Note: Allocate some space for the Bytes, as 13-element array of bytes
 unsigned char command[16];  // Anthony Note: Allocate some space for the Bytes, as 16-element array of bytes
-float Temperature, VarXvalue;
-unsigned char startmeasure[8] = {0x01, 0x03, 0x25, 0x00, 0x00, 0x01, 0x8F, 0x06};
-unsigned char getTempandVarX[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from Turbidity manual
-//unsigned char getTempandTurb[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from DO & Turbidity manual
-//unsigned char getTempandDO[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from DO & Turbidity manual
-//unsigned char getTempandChl[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x05, 0x8E, 0x81};  // from DO & Turbidity manual
+float Temperature, VarXvalue, SN;
 
+unsigned char startmeasure[8] = {0x01, 0x03, 0x25, 0x00, 0x00, 0x01, 0x8F, 0x06};  // from Turb code
+// unsigned char startmeasure[8] = {0x01, 0x03, 0x25, 0x00, 0x00, 0x00, 0x4E, 0xC6};  // from Turb manual
+unsigned char getTempandVarX[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};  // from Turbidity manual
+
+unsigned char getSN[8] = {0x01, 0x03, 0x09, 0x00, 0x00, 0x07, 0x07, 0x94};
+unsigned char activateBrush[9] = {0x01, 0x10, 0x31, 0x00, 0x00, 0x00, 0x00, 0x74, 0x94};
 int i = 0;      // Anthony note: Index into array; where to store the Bytes
 int inbyte;     // Anthony note: Where to store the Bytes read
 String inputString = "";
@@ -59,7 +62,9 @@ void setup()
     // read the incoming byte:
     incomingByte = Serial1.readBytes(buffer, 13);
   }
+  //Serial.println(buffer[2]);
   Serial.println("Temp(C)   TUR(NTU)");
+  //Serial.print("Sesnor SN "); Serial.println(SN); //Beth note: trying to print serial number in header
 }
 
 void loop()
@@ -72,7 +77,6 @@ void loop()
   else {
     State8 = LOW;
   }
-
 
   digitalWrite(8, State8);  // Anthony Note: Turn on LED2 green if State8 is high
   State9 = !State8;         // Anthony Note: Assign State9 to be NOT State8 (the opposite of State8)
@@ -94,14 +98,14 @@ void loop()
   {
     // read the incoming byte:
     incomingByte = Serial1.readBytes(buffer, 13); //default to 1 second
-  // say what you got:
+    // say what you got:
     if (incomingByte == 13)
     {
-      Temperature = Rev_float(buffer, 3);
-      VarXvalue = Rev_float(buffer, 7);
-      Serial.print(Temperature, 2);
-      Serial.print("   ");
-      Serial.println(VarXvalue, 2);
+      Temperature = Rev_float(buffer, 3);  // Anthony note: read response frame buffer starting at byte 3
+      VarXvalue = Rev_float(buffer, 7);    // Anthony note: read response frame buffer starting at byte 7
+      Serial.print(Temperature, 6);
+      Serial.print(", ");
+      Serial.println(VarXvalue, 6);
     }
   }
 }
