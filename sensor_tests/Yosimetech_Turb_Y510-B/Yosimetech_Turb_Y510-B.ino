a001372ba327dbf0a649587a5ec76d6348dfd7fb
SRGDamia1 <sdamiano@stroudcenter.org>
2017-06-27 14:38:33 -0400

    Lots of improvements for Y510-B
    -More vars at top
    -timeouts
    -warmup
    -extra debugging
    -device/receiver enable

--- sensor_tests/Yosimetech_Turb_Y510-B/Yosimetech_Turb_Y510-B.ino
+++ sensor_tests/Yosimetech_Turb_Y510-B/Yosimetech_Turb_Y510-B.ino
@@ -1,7 +1,7 @@
 /*****************************************************************************
 Modified by Anthony & Beth
 From sketch from YosemiTech for
-Y511 Turbidity with wiper
+Y510-B Turbidity with wiper
 *****************************************************************************/
 
 // ---------------------------------------------------------------------------
@@ -13,51 +13,94 @@ Y511 Turbidity with wiper
 // Set up the sensor specific information
 //   ie, pin locations, addresses, calibrations and related settings
 // ---------------------------------------------------------------------------
-// Anthony note: Declare variables
-int State8 = LOW;
-int State9 = LOW;
-int incomingByte = 0; // for incoming serial data. Anthony Note: where to store the bytes read
-// Anthony Note: "unsigned char" datatype is equivalent to "byte". https://oscarliang.com/arduino-difference-byte-uint8-t-unsigned-cha/
-unsigned char buffer[13];   // Anthony Note: Allocate some space for the Bytes, as 13-element array of bytes
-unsigned char command[16];  // Anthony Note: Allocate some space for the Bytes, as 16-element array of bytes
-float Temperature, VarXvalue, SN;
-unsigned char startmeasure[8]   = {0x01, 0x03, 0x25, 0x00, 0x00, 0x00, 0x4E, 0xC6};
-                                // Addr, Fxn , Start Addr, # Register,    CRC
-                                //   01, Read, Coil 9472 ,   0 Regs  ,    CRC
-unsigned char getTempandVarX[8] = {0x01, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};
-                                // Addr, Fxn , Start Addr, # Register,    CRC
-                                //   01, Read, Coil 9728 ,   4 Regs  ,    CRC
-unsigned char getSN[8] =          {0x01, 0x03, 0x09, 0x00, 0x00, 0x07, 0x07, 0x94};
-                               // Addr, Fxn , Start Addr, # Register,    CRC
-                               //   01, Read, Coil 2304 ,   7 Regs  ,    CRC
-int i = 0;      // Anthony note: Index into array; where to store the Bytes
-int inbyte;     // Anthony note: Where to store the Bytes read
-String inputString = "";
+
+// Define pin number variables
+const int PwrPin = 22;  // The pin sending power to the sensor AND RS485 adapter
+const int DEREPin = -1;   // The pin controlling Recieve Enable and Driver Enable
+                          // on the RS485 adapter, if applicable (else, -1)
+                          // Setting HIGH enables the driver (arduino) to send text
+                          // Setting LOW enables the receiver (sensor) to send text
+const int SSRxPin = 10;  // Recieve pin for software serial (Rx on RS485 adapter)
+const int SSTxPin = 11;  // Send pin for software serial (Tx on RS485 adapter)
+
+// Define the sensor addresses
+unsigned char modbusAddress = 0x01;
+const int modbusTimeout = 1000;  // time to wait for response in ms
+
+#include <SoftwareSerial.h>
+SoftwareSerial modbusSerial(SSRxPin, SSTxPin);
+
+// Define arrays with the modbus commands
+unsigned char startMeasurement[8] = {modbusAddress, 0x03, 0x25, 0x00, 0x00, 0x00, 0x4E, 0xC6};
+                                  // Address      , Fxn , Start Addr, # Register,    CRC
+                                  // modbusAddress, Read, Coil 9472 ,   0 Regs  ,    CRC
+unsigned char getTempandVarX[8] = {modbusAddress, 0x03, 0x26, 0x00, 0x00, 0x04, 0x4F, 0x41};
+                                // Address      , Fxn , Start Addr, # Register,    CRC
+                                // modbusAddress, Read, Coil 9728 ,   4 Regs  ,    CRC
+unsigned char getSN[8] = {modbusAddress, 0x03, 0x09, 0x00, 0x00, 0x07, 0x07, 0x94};
+                       // Address      , Fxn , Start Addr, # Register,    CRC
+                       // modbusAddress, Read, Coil 2304 ,   7 Regs  ,    CRC
+unsigned char stopMeasurement[8] = {modbusAddress, 0x03, 0x2E, 0x00, 0x00, 0x00, 0x4C, 0xE2};
+                                 // Address      , Fxn , Start Addr, # Register,    CRC
+                                 // modbusAddress, Read, Coil 11776,   0 Regs  ,    CRC
+// Define variables for the response;
+uint32_t start;  // For time-outs
+uint32_t warmup;  // For time-outs
+int bytesRead;
+unsigned char responseBuffer[20];  // This needs to be bigger than the largest response
+
+// Define variables to hold the float values calculated from the response
+float Temperature, VarXvalue;
+String SN;
+
 
 // ---------------------------------------------------------------------------
 // Working Functions
 // ---------------------------------------------------------------------------
+
+// Define a small-endian frame as a union - that is a special class type that
+// can hold only one of its non-static data members at a time, in this case,
+// either 4-bytes OR a single float.
+// With avr-gcc (Arduino's compiler), integer and floating point variables are
+// all physically stored in memory in little-endian byte order, so this union
+// is all that is needed to get the correct float value from the small-endian
+// hex frames returned by YosemiTech's Modbus Sensors
 union SeFrame {
   float Float;
   unsigned char Byte[4];
 };
 
-SeFrame Sefram;
+// This functions return the float from a 4-byte small-endian array beginning
+// at a specific index of another array.
 float Rev_float( unsigned char indata[], int stindex)
 {
-    Sefram.Byte[0] = indata[stindex];//Serial.read( );
-    Sefram.Byte[1] = indata[stindex + 1]; //Serial.read( );
-    Sefram.Byte[2] = indata[stindex + 2]; //Serial.read( );
-    Sefram.Byte[3] = indata[stindex + 3]; //Serial.read( );
+    SeFrame Sefram;
+    Sefram.Byte[0] = indata[stindex];
+    Sefram.Byte[1] = indata[stindex + 1];
+    Sefram.Byte[2] = indata[stindex + 2];
+    Sefram.Byte[3] = indata[stindex + 3];
     return Sefram.Float;
 }
 
-float Rev_float(unsigned char indata[], int stindex);
+// This flips the device/receive enable to DRIVER so the arduino can send text
+void driverEnable(void)
+{
+    if (DEREPin > 0)
+    {
+        digitalWrite(DEREPin, HIGH);
+        delay(8);
+    }
+}
 
-
-#include <SoftwareSerial.h>
-SoftwareSerial modbusSerial(10, 11);  // Picking these pins for software serial
-
+// This flips the device/receive enable to RECIEVER so the sensor can send text
+void recieverEnable(void)
+{
+    if (DEREPin > 0)
+    {
+        digitalWrite(DEREPin, LOW);
+        delay(8);
+    }
+}
 
 
 // ---------------------------------------------------------------------------
@@ -65,56 +108,96 @@ SoftwareSerial modbusSerial(10, 11);  // Picking these pins for software serial
 // ---------------------------------------------------------------------------
 void setup()
 {
-    // set initial pin modes
-    pinMode(8, OUTPUT);   // Anthony Note: LED2 green
-    pinMode(9, OUTPUT);   // Anthony Note: LED1 red
 
-    pinMode(22, OUTPUT);  // Anthony Note: switched power. 5V to sensor, 3.3V to RS485 adaptor
-    digitalWrite(22, HIGH);
+    pinMode(PwrPin, OUTPUT);
+    digitalWrite(PwrPin, HIGH);
 
-    // pinMode(12, OUTPUT);  // SRGD Note: Receive enable/Data enable, iff using MAX485
-    // digitalWrite(12, LOW);  // Setting LOW enables the receiver (sensor) to send text
+    if (DEREPin > 0) pinMode(DEREPin, OUTPUT);
 
-    Serial.begin(9600);  // Anthony Note: this is the Mayfly's default USB port (UART-0)
-    // Serial1.begin(9600);  // this is the Mayfly's default Xbee port (UART-1)
+    Serial.begin(9600);  // Main serial port for debugging
     modbusSerial.begin(9600);
 
-    // digitalWrite(12, HIGH);  // Setting HIGH enables the driver (mayfly) to send text
-    // delay(8);  // A short delay for the DE/RE switch
+    // Allow the sensor and converter to warm up
+    delay(500);
 
-    // modbusSerial.write(getSlaveAddress, 8);  // Send the "getSlaveAddress" command
-    modbusSerial.write(startmeasure, 8);  // Send the "start measurement" command
+    // Send the "get serial number" command
+    driverEnable();
+    modbusSerial.write(getSN, 8);
     modbusSerial.flush();
 
-    // digitalWrite(12, LOW);  // Setting LOW enables the receiver (sensor) to send text
-    // delay(8);  // A short delay for the DE/RE switch
+    recieverEnable();
+    start = millis();
+    while (modbusSerial.available() == 0 && millis() - start < modbusTimeout)
+    { delay(1);}
 
-    uint8_t attempts = 1;
-    while (attempts < 11)
+    if (modbusSerial.available() > 0)
     {
-        uint32_t start = millis();
-        while (modbusSerial.available() == 0 && millis() - start < 2000)
-        { delay(1);}
+        // read the incoming byte:
+        bytesRead = modbusSerial.readBytes(responseBuffer, 18);
 
-        if (modbusSerial.available() > 0)
+        // print the raw response (for debugging)
+        // Serial.print("Raw SN Response (");
+        // Serial.print(bytesRead);
+        // Serial.print(" bytes):");
+        // for (int i = 0; i < bytesRead; i++) Serial.print(responseBuffer[i]);
+        // Serial.println();
+
+        // Parse into a string and print that
+        if (bytesRead == 18)
         {
-            // read the incoming byte:
-            incomingByte = modbusSerial.readBytes(buffer, 13);
-            Serial.print("Response: ");
-            Serial.println(incomingByte);
-            attempts = 11;
-        }
-        else
-        {
-            Serial.print("No response to attempt #");
-            Serial.println(attempts);
-            attempts++;
+            int sn_len = responseBuffer[2];
+            char sn_arr[sn_len] = {0,};
+            int j = 0;
+            for (int i = 4; i < 16; i++)
+            {
+                sn_arr[j] = responseBuffer[i];
+                j++;
+            }
+            SN = String(sn_arr);
+            Serial.print("Serial Number: ");
+            Serial.println(SN);
         }
     }
+    else
+    {
+        Serial.println("No response to serial number request");
+    }
 
-    delay(2000);  // recommended >2 second delay (see p 15 of manual) after Start Meaurement before Get values
+    // Send the "start measurement" command
+    driverEnable();
+    modbusSerial.write(startMeasurement, 8);
+    modbusSerial.flush();
 
-    Serial.println("Temp(C)   TUR(NTU)");
+    recieverEnable();
+    start = millis();
+    while (modbusSerial.available() == 0 && millis() - start < modbusTimeout)
+    { delay(1);}
+
+    if (modbusSerial.available() > 0)
+    {
+        // read the incoming byte:
+        bytesRead = modbusSerial.readBytes(responseBuffer, 18);
+        warmup = millis();
+
+        // print the raw response (for debugging)
+        Serial.print("Raw Start Measurement Response (");
+        Serial.print(bytesRead);
+        Serial.print(" bytes):");
+        for (int i = 0; i < bytesRead; i++) Serial.print(responseBuffer[i], HEX);
+        Serial.println();
+    }
+    else
+    {
+        Serial.println("No response to Start Measurement request");
+    }
+
+    // Modbus manual (p15) recommendeds >2 second delay after Start Meaurement
+    // before requesting values.  My testing actually indicates that turbidity
+    // returns 0 until about 5-6 seconds after the start measurement command.
+    // It may take up to 22 seconds to get stable values.
+    // delay(5000);
+
+    Serial.println("Temp(C)  Turb(NTU)  Warmup(ms)");
 }
 
 // ---------------------------------------------------------------------------
@@ -122,49 +205,44 @@ void setup()
 // ---------------------------------------------------------------------------
 void loop()
 {
-    // Anthony Note: Switch the LED state
-    // if (State8 == LOW)
-    // {
-    //     State8 = HIGH;
-    // }
-    // else {
-    //     State8 = LOW;
-    // }
-    //
-    // digitalWrite(8, State8);  // Anthony Note: Turn on LED2 green if State8 is high
-    // State9 = !State8;         // Anthony Note: Assign State9 to be NOT State8 (the opposite of State8)
-    // digitalWrite(9, State9);  // Anthony Note: Turn on LED1 red if State9 is high
-
-
     // send the command to get the temperature
-    // digitalWrite(12, HIGH);  // Setting HIGH enables the driver (mayfly) to send text
-    // delay(8);  // A short delay for the DE/RE switch
+    driverEnable();
     modbusSerial.write(getTempandVarX, 8);
     modbusSerial.flush();
 
-
-    // digitalWrite(12, LOW);  // Setting LOW enables the receiver (sensor) to send text
-
-    uint32_t start = millis();
-    while (modbusSerial.available() == 0 && millis() - start < 2000)
+    recieverEnable();
+    start = millis();
+    while (modbusSerial.available() == 0 && millis() - start < modbusTimeout)
     { delay(1);}
 
     if (modbusSerial.available() > 0)
     {
         // read the incoming byte:
-        incomingByte = modbusSerial.readBytes(buffer, 13); //default to 1 second
-        Serial.println(incomingByte);
-        // say what you got:
-        if (incomingByte == 13)
+        bytesRead = modbusSerial.readBytes(responseBuffer, 18);
+
+        // print the raw response (for debugging)
+        // Serial.print("Raw Get Value Response (");
+        // Serial.print(bytesRead);
+        // Serial.print(" bytes):");
+        // for (int i = 0; i < bytesRead; i++) Serial.print(responseBuffer[i], HEX);
+        // Serial.println();
+
+        // Print response converted to floats
+        if (bytesRead == 13)
         {
-            Temperature = Rev_float(buffer, 3);
-            VarXvalue = Rev_float(buffer, 7);
+            Temperature = Rev_float(responseBuffer, 3);
+            VarXvalue = Rev_float(responseBuffer, 7);
             Serial.print(Temperature, 2);
-            Serial.print("   ");
-            Serial.println(VarXvalue, 2);
+            Serial.print("     ");
+            Serial.print(VarXvalue, 2);
+            Serial.print("       ");
+            Serial.print(millis() - warmup);
+            Serial.println();
         }
     }
-    else Serial.println("   -         -");
+    else Serial.println("  -         -           -");
 
-    delay(2000);  // Delay 1 second for response
+    // Delay between readings
+    // Sensor returns values no more frequently than once per second
+    // delay(1000);
 }
